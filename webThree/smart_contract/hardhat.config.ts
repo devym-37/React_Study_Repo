f627abab052afef4565e0e242f57193fa2e8a056
miniyoung <miniyoung37@gmail.com>
2023-03-19 20:05:03 +0900

    # web3.0 deploy hardhat.config 수정

--- webThree/smart_contract/hardhat.config.ts
+++ webThree/smart_contract/hardhat.config.ts
@@ -1,8 +1,15 @@
+// https://eth-goerli.g.alchemy.com/v2/mKjmyXRhsHS9yC_nIBReiWlzJRwOvspm
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+    solidity: "0.8.18",
+    networks: {
+        goelri: {
+            url: "https://eth-goerli.g.alchemy.com/v2/mKjmyXRhsHS9yC_nIBReiWlzJRwOvspm",
+            accounts: ["76dc7bbd09b43ff4d45ed280e297f01ecb14aefae0edb492dd598432cbccae25"],
+        },
+    },
 };
 
 export default config;
