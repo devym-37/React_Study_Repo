72f4efb1a5026476d91d62ad2391e1aaaf1197f9
Lee YoungMin <miniyoung37@gmail.com>
2023-03-22 00:04:16 +0900

    # web3.0 context 구성

---
+++ webThree/client/src/context/TransactionContext.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+import { contractAbi, contractAddress } from "../utils/constants";
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+
+    const transactionContract = new ethers.Contract(contractAddress, contractAbi, signer);
+
+    console.log({ provider, signer, transactionContract });
+};
+
+export const TransactionProvider = ({ children }) => {
+    return <TransactionContext.Provider value={{}}>{children}</TransactionContext.Provider>;
+};
