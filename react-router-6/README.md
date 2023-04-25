4cad58c16a71560dbbe7e4d5113fb6f6962806c9
Lee YoungMin <miniyoung37@gmail.com>
2023-04-26 00:10:22 +0900

    useNavigate

--- react-router-6/README.md
+++ react-router-6/README.md
@@ -1,3 +1,9 @@
 # React Router Guide
 
 ### BrowserRouter
+
+### createBrowserRouter
+
+### errorElement
+
+### useNavigate
--- react-router-6/src/components/Header.tsx
+++ react-router-6/src/components/Header.tsx
@@ -1,6 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const onClick = () => {
+        navigate("/about");
+    };
+
     return (
         <h1>
             <ul>
@@ -8,7 +14,7 @@ const Header = () => {
                     <Link to={"/"}>Home</Link>
                 </li>
                 <li>
-                    <Link to={"/about"}>About</Link>
+                    <button onClick={onClick}>About</button>
                 </li>
             </ul>
         </h1>
