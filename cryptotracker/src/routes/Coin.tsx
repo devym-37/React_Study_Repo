f690776e031e46fffb881d14083379c264b13285
Lee YoungMin <miniyoung37@gmail.com>
2023-05-01 21:22:47 +0900

    styled component 추가

--- cryptotracker/src/routes/Coin.tsx
+++ cryptotracker/src/routes/Coin.tsx
@@ -1,15 +1,20 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 interface RouteParams {
   coinId: string;
 }
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
 
 const Coin = () => {
   const { coinId } = useParams() as {
     coinId: string;
   };
 
-  return <h1>Coin</h1>;
+  return <Title>Coin</Title>;
 };
 
 export default Coin;
