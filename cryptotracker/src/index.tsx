75fe2cd9f12f12055e743fcd2b6324e3a1e25abd
Lee YoungMin <miniyoung37@gmail.com>
2023-05-05 22:22:32 +0900

    add react-query

--- cryptotracker/src/index.tsx
+++ cryptotracker/src/index.tsx
@@ -4,16 +4,21 @@ import App from "./App";
 import { theme } from "./theme";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
 import router from "./Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
