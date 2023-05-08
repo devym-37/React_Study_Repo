aa46296a714cb889407a7f3b4d7ab8ac221e43f7
Lee YoungMin <miniyoung37@gmail.com>
2023-05-08 23:02:55 +0900

    init trello clone

---
+++ trello-clone/src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import App from "./App";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const queryClient = new QueryClient();
+
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
