6cc74854375d824386ed70557f08cfb445120622
Lee Young Min <miniyoung37@gmail.com>
2023-10-01 21:46:31 +0900

    update State 방식

--- react-router-6/src/screens/Home.tsx
+++ react-router-6/src/screens/Home.tsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
 import { users } from "../dummy";
-import { UserStoreType, userStore } from "../store/zustandStore";
+import {
+  UserStoreType,
+  usePersonState,
+  userStore,
+} from "../store/zustandStore";
 
 const Home = () => {
   const userCount = userStore((state: UserStoreType) => state.userCount);
+
+  const firstName = usePersonState((state) => state.firstName);
+  const updateFirstName = usePersonState((state) => state.updateFirstName);
+
   console.log("userCount", userCount);
   return (
     <div>
@@ -15,6 +23,10 @@ const Home = () => {
           </li>
         ))}
       </ul>
+      <input
+        onChange={(e) => updateFirstName(e.target.value)}
+        value={firstName}
+      />
     </div>
   );
 };
--- react-router-6/src/store/zustandStore.ts
+++ react-router-6/src/store/zustandStore.ts
@@ -9,3 +9,20 @@ export const userStore = create<UserStoreType>((set) => ({
   increaseUserCount: () => set((state) => ({ userCount: state.userCount + 1 })),
   removeUserCount: () => set({ userCount: 0 }),
 }));
+
+type State = {
+  firstName: string;
+  lastName: string;
+};
+
+type Action = {
+  updateFirstName: (firstName: State["firstName"]) => void;
+  updateLastName: (lastName: State["lastName"]) => void;
+};
+
+export const usePersonState = create<State & Action>((set) => ({
+  firstName: "",
+  lastName: "",
+  updateFirstName: (firstName) => set({ firstName }),
+  updateLastName: (lastName) => set({ lastName }),
+}));
