da6adc245b6bf3b7c76a2434b7286105a35f2a35
LeeYoungMin <miniyoung37@gmail.com>
2024-07-28 22:18:45 +0900

    router 개념 설명

---
+++ learn-nextjs14/app/about-us/company/sales/page.tsx
@@ -0,0 +1,12 @@
+export default function Sales() {
+  return (
+    <div>
+      <h1>Sales</h1>
+      <p>
+        Our sales team is always ready to help you with any questions you may
+        have about our products and services. We are here to make sure you get
+        the best possible experience when working with us.
+      </p>
+    </div>
+  );
+}
---
+++ learn-nextjs14/app/about-us/page.tsx
@@ -0,0 +1,13 @@
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        We are a team of passionate developers who love to build web
+        applications using modern technologies. We are always looking for new
+        ways to improve our skills and deliver the best possible experience to
+        our users.
+      </p>
+    </div>
+  );
+}
