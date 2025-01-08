75293b4e08a7a9288c0c5817d507cdcfc65744bf
LeeYoungMin <miniyoung37@gmail.com>
2025-01-08 23:12:20 +0900

    bubbleSort 구현 코드

---
+++ algorithm/bubbleSort.mjs
@@ -0,0 +1,11 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+};
+
+export { bubbleSort };
---
+++ algorithm/test_bubbleSort.mjs
@@ -0,0 +1,7 @@
+import { bubbleSort } from "./bubbleSort.mjs";
+
+const arr = [3, 2, 1, 4];
+
+bubbleSort(arr);
+
+console.log(arr); // [1, 2, 3, 4]
